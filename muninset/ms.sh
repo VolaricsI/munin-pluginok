29b52901f25ef97e8df6a33852fe6992d532b5b3
Volarics István <volaricsi@volarics.hu>
2021-03-21 06:53:54 +0100

    Szépészet

--- muninset/ms.sh
+++ muninset/ms.sh
@@ -71,28 +71,28 @@ Cikl(){
 #Cikl 'tu20.volarics.hu-*diskstats_utilization*' 		100 felett 	NaN
 #Cikl 'tu20.volarics.hu-*diskstats_throughput*' 		1e9 felett 	NaN
 
-Cikl 'tu20.volarics.hu-*diskstats_throughput-sdn-*' 		1e9 felett 	NaN
+#Cikl 'tu20.volarics.hu-*diskstats_throughput-sdn-*' 		1e9 felett 	NaN
 
+Cikl 'tu20.volarics.hu-Sensors_fan-fan*'		 	2000 felett NaN
 
 #Setting 	tu20.volarics.hu-processes-zombie-g.rrd 	100 felett NaN
 
+#Setting 	tu20.volarics.hu-sensors_fan-fan1-g.rrd 	2000 felett NaN
+#Setting 	tu20.volarics.hu-sensors_fan-fan2-g.rrd 	2000 felett NaN
+#Setting 	tu20.volarics.hu-sensors_fan-fan3-g.rrd 	2000 felett NaN
+#Setting 	tu20.volarics.hu-sensors_fan-fan4-g.rrd 	2000 felett NaN
 #Setting 	tu20.volarics.hu-sensors_fan-fan5-g.rrd 	2000 felett NaN
 
 #Setting 	tu20.volarics.hu-vg_vgtu20sys-free-g.rrd 	300e9 felett NaN
 #Setting 	tu20.volarics.hu-vg_vgtu20sys-size-g.rrd 	300e9 felett NaN
 #Setting 	tu20.volarics.hu-vg_vgtu20sys-lv_root-g.rrd 	300e9 felett NaN
 
-
-Setting 	tu20.volarics.hu-docker_memory-rTorrent-g.rrd 	5e9 felett NaN
+#Setting 	tu20.volarics.hu-docker_memory-rTorrent-g.rrd 	5e9 felett NaN
 
 #Setting 	tu20.volarics.hu-sensors_temp-temp4-g.rrd 	23 alatt NaN
-#Setting 	tu20.volarics.hu-Sensors_temp-temp4-g.rrd 	23 alatt NaN
 #Setting 	tu20.volarics.hu-sensors_temp-temp5-g.rrd 	23 alatt NaN
-#Setting 	tu20.volarics.hu-Sensors_temp-temp5-g.rrd 	23 alatt NaN
 #Setting 	tu20.volarics.hu-sensors_temp-temp6-g.rrd 	23 alatt NaN
-#Setting 	tu20.volarics.hu-Sensors_temp-temp6-g.rrd 	23 alatt NaN
 #Setting 	tu20.volarics.hu-sensors_temp-temp8-g.rrd 	23 alatt NaN
-#Setting 	tu20.volarics.hu-Sensors_temp-temp8-g.rrd 	23 alatt NaN
 
 #Setting tu20.volarics.hu-diskstats_throughput-md126_rdbytes-g.rrd 	MaxFelett 	450.000.000 #NaN
 #Setting tu20.volarics.hu-diskstats_throughput-md126_wrbytes-g.rrd 	MaxFelett 	450000000 #NaN
